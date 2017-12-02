51261338b6eea0174902f9574d9e10e2928e490d
Colin Zhang <l8v0b@ugrad.cs.ubc.ca>
2017-12-02 15:35:17 -0800

    Made parser, some bullshit

--- src/Parsers/CurrencyParser.java
+++ src/Parsers/CurrencyParser.java
@@ -1,9 +1,38 @@
 package Parsers;
 
+import model.CurrencyExchange;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Currency;
+
 public class CurrencyParser {
 
-    public void parseCurrency(String jsonResponse) {
+    public void parseCurrency(String jsonResponse, Currency toCurrency) throws JSONException {
+
+        JSONObject currency = new JSONObject(jsonResponse);
+        String fromCurrencyCode = currency.getString("base");
+        JSONObject rates = currency.getJSONObject("rates");
+        Double exchangeRate = rates.getDouble(toCurrency.toString());
+
+        CurrencyExchange ce = new CurrencyExchange();
+        //ce.setFromCurrency();
+        ce.setToCurrency(toCurrency);
+        ce.setExchangeRate(exchangeRate);
+
 
 
     }
 }
+
+//{
+//        "Realtime model.Currency Exchange Rate": {
+//        "1. From_Currency Code": "USD",
+//        "2. From_Currency Name": "United States Dollar",
+//        "3. To_Currency Code": "JPY",
+//        "4. To_Currency Name": "Japanese Yen",
+//        "5. Exchange Rate": "112.12500000",
+//        "6. Last Refreshed": "2017-12-02 21:56:18",
+//        "7. Time Zone": "UTC"
+//        }
+//        }
---
+++ src/auth/APIToken.java
@@ -0,0 +1,5 @@
+package auth;
+
+public class APIToken {
+    public static final String API_KEY = "X0J4X1UA0GILEVEL";
+}
