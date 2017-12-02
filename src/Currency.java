38e8210a44b03383924a8e0314e2290761937057
John Sastrillo <john.sastrillo@gmail.com>
2017-12-02 15:30:28 -0800

    This code is fucked

---
+++ src/Currency.java
@@ -0,0 +1,8 @@
+/**
+ * Created by john_ on 2017-12-02.
+ */
+enum Currencies {
+    AUD, BGN, BRL, CAD, CHF, CNY, CZK, DKK, GBP, HKD, HRK, HUF, IDR, ILS, INR, JPY, KRW, MXN, MYR, NOK, NZD, PHP, PLN, RON, RUB, SEK, SGD, THB, TRY, ZAR, EUR
+
+}
+
